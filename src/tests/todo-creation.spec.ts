fbbbe94bf2c4213c8c27d4a07a53000bf64bd63b
SasaRuu <sasanika@expernetic.com>
2025-09-04 16:41:05 +0530

    changed neww

--- src/tests/todo-creation.spec.ts
+++ src/tests/todo-creation.spec.ts
@@ -1,9 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { TODO_ITEMS } from "./todo-items";
-import {
-    checkNumberOfTodosInLocalStorage,
-    createDefaultTodos
-} from "../utils/utils";
+import { checkNumberOfTodosInLocalStorage, createDefaultTodos } from "../utils/utils";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("https://demo.playwright.dev/todomvc");
@@ -19,39 +16,29 @@ test.describe("New Todo", () => {
 
     await newTodo.fill(TODO_ITEMS[1]);
     await newTodo.press("Enter");
-    await expect(page.getByTestId("todo-title")).toHaveText([
-      TODO_ITEMS[0],
-      TODO_ITEMS[1],
-    ]);
+    await expect(page.getByTestId("todo-title")).toHaveText([TODO_ITEMS[0], TODO_ITEMS[1]]);
 
     await checkNumberOfTodosInLocalStorage(page, 2);
   });
 
-  test("should clear text input field when an item is added", { tag: ['@T6084'] }, async ({
-    page,
-  }) => {
+  test("should clear text input field when an item is added", { tag: ['@T6084'] }, async ({ page }) => {
     const newTodo = page.getByPlaceholder("What needs to be done?");
 
     await newTodo.fill(TODO_ITEMS[0]);
     await newTodo.press("Enter");
-    // Purposely fail: Expect input NOT to be empty
-    await expect(newTodo).not.toBeEmpty();
+    // After adding, the input SHOULD be empty
+    await expect(newTodo).toBeEmpty();
     await checkNumberOfTodosInLocalStorage(page, 1);
   });
 
-  test("should append new items to the bottom of the list", { tag: ['@T6085'] }, async ({
-    page,
-    browserName,
-  }) => {
+  test("should append new items to the bottom of the list", { tag: ['@T6085'] }, async ({ page, browserName }) => {
     test.skip(browserName === "firefox", "Not running on Firefox");
 
     await createDefaultTodos(page);
     const todoCount = page.getByTestId("todo-count");
 
     await expect(page.getByText("3 items left")).toBeVisible();
-    await expect(todoCount).toHaveText("3 items left");
-    await expect(todoCount).toContainText("2");
-    await expect(todoCount).toHaveText(/3/);
+    await expect(todoCount).toHaveText("3 items left");   // keep it consistent
     await expect(page.getByTestId("todo-title")).toHaveText(TODO_ITEMS);
     await checkNumberOfTodosInLocalStorage(page, 3);
   });
